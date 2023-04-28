a3d62add8d5e16ab7e7cb39a87794fac152a7383
Jeff Uchitjil <git@k9kld.org>
2023-04-27 21:30:00 -0500

    Refactor into more generic utility project
    * Rename from Maidenhead to CoreServices to accomodate several services.
    * Fix up all project and solution files to match new naming structure.

---
+++ CoreServices/Model/Distance.cs
@@ -0,0 +1,19 @@
+namespace CoreServices.Model;
+
+public class Distance
+{
+    public Distance()
+    {
+        //Needed to serialize as xml
+    }
+
+    public Distance(double kilometers)
+    {
+        Kilometers = (int)Math.Round(kilometers, 0, MidpointRounding.AwayFromZero);
+        Miles = (int)Math.Round(kilometers * .6214, 0, MidpointRounding.AwayFromZero);
+    }
+
+    public int Miles { get; set; }
+
+    public int Kilometers { get; set; }
+}
